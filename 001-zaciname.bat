5658fc59e209eea3f057a42ba7b8c56b4a33370e
Tibor Repček <tiborepcek@gmail.com>
2017-01-20 19:28:38 +0100

    Update 001-zaciname.bat

--- 001-zaciname.bat
+++ 001-zaciname.bat
@@ -1,4 +1,5 @@
 REM Vytvoríme si nový priečinok a presunieme doň všetky fotky s koncovkou jpg
+REM Ako by to vyzeralo, keby som chcel fotky skopírovať (nie presunúť)?
 
 mkdir fotky
 move *.jpg fotky
