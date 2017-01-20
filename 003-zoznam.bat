19df9417d31a787a2a7915423e85f280f475f863
Tibor Repček <tiborepcek@gmail.com>
2017-01-20 19:43:51 +0100

    Create 003-zoznam.bat

---
+++ 003-zoznam.bat
@@ -0,0 +1,8 @@
+REM Povedzme, že v priečinku máte desiatky legálne nedobudnutých filmov a potrebujete ich zoznam.
+REM Stačí jediný riadok, aby sme zoznam filmov vypísali do textového súboru:
+
+dir > zoznam.txt
+
+REM Alebo ak potrebujete vypísať iba názvy súborov bez ostatných informácií, použite prepínač /b takto:
+
+dir /b > zoznam.txt
