9b3aaf373bc5bb5be3ef3a5b6f4b822336ed5a48
Tibor Repček <tiborepcek@gmail.com>
2017-01-20 19:57:05 +0100

    Create 004-incognito.bat

---
+++ 004-incognito.bat
@@ -0,0 +1,4 @@
+REM V skripte môžete spustiť aj externý program ako Microsoft Internet Explorer alebo Google Chrome so špeciálnymi prepínačmi.
+
+start iexplore.exe -private
+start chrome --incognito
